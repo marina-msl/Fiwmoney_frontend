053ddbe8895b6b6d1eb527d8ba086f8467d4f301
Marina Leide <marina.msleide@gmail.com>
2025-06-05 21:38:29 -0300

    Implementing notify as a switch

--- src/components/StockCard.vue
+++ src/components/StockCard.vue
@@ -5,6 +5,13 @@
         <p><strong>Preço Médio:</strong> {{ formatCurrency(stock.averagePrice) }}</p>
         <p><strong>Diferença: </strong>  {{ formatCurrency(getDifference(stock)) }}</p>
         <p><strong>Status: </strong>     {{ getStatus(stock) }}</p>    
+        <p>
+          <strong>Notificar: </strong>
+          <label class="switch">
+          <input type="checkbox" v-model="notify" @change="toggleNotify" />
+          <span class="slider"></span>
+          </label>
+        </p>
     </div> 
   </template>
 
@@ -32,10 +39,6 @@
         getStatus(stock) {
          return this.getDifference(stock) >= 0 ? "Esperar 🔴" :  "Comprar! 🟢";
         }
-
-        // getNotify(stock) {
-        //     return true;
-        // }
 
     }   
 };
@@ -65,6 +68,51 @@ h2 {
 p {
   margin: 6px 0;
   font-size: 1rem;
+}
+
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 33px;
+  height: 18px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  background-color: #ccc;
+  transition: 0.4s;
+  border-radius: 15px;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 10px;
+  width: 10px;
+  border-radius: 50%;
+  background-color: white;
+  transition: 0.2s;
+  top: 3px;
+  left: 3px;
+}
+
+input:checked + .slider {
+  background-color: #1abc9c;
+}
+
+input:checked + .slider:before {
+  transform: translateX(18px);
 }
 
 </style>
